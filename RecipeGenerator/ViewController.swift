15e3c5565887380b6b7e18cee62e7dd6d459403d
jorted-runner <jorted.python@gmail.com>
2025-02-11 10:59:02 -0600

    Initial Commit

---
+++ RecipeGenerator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RecipeGenerator
+//
+//  Created by Danny Ellis on 2/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
